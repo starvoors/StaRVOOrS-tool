24b9c14f5881fd6146ad1f50e44be49811e2be60
Mauricio Chimento <jmau.chimento@gmail.com>
2019-08-15 15:07:15 +0200

    upd

--- test/TestSuite.hs
+++ test/TestSuite.hs
@@ -16,7 +16,7 @@ testSuite =
 
 data Option = Yes | No deriving(Eq,Show)
 
-type Program = String
+type Program = [String]
 
 data Test = 
  Test { _name          :: String --Name of the test
@@ -36,29 +36,29 @@ test_automata_communication_1 =
  Test "automata_communication_case_1" "automata_communication.ppd" No Yes No (Yes,prog1)
 
 prog1 :: Program
-prog1 =
-    "CMachine cm = new CMachine(2);\n\n"
- ++ "cm.brew();\n"
- ++ "cm.brew();\n"
- ++ "cm.cleanF();\n"
- ++ "cm.brew();\n"
- ++ "cm.brew();\n"
+prog1 = [ "CMachine cm = new CMachine(2);\n\n"
+        , "cm.brew();\n"
+        , "cm.brew();\n"
+        , "cm.cleanF();\n"
+        , "cm.brew();\n"
+        , "cm.brew();\n"
+        ]
 
 test_grammar :: Test
-test_grammar = Test "grammar" "grammar.ppd" Yes No No (No,"")
+test_grammar = Test "grammar" "grammar.ppd" Yes No No (No,[])
 
 test_hoare_triples_behaviour :: Test
 test_hoare_triples_behaviour = 
- Test "hoare_triples_behaviour" "hoare_triples_behaviour.ppd" No Yes Yes (Yes,"")
+ Test "hoare_triples_behaviour" "hoare_triples_behaviour.ppd" No Yes Yes (Yes,[])
 
 test_monitor_behaviour :: Test
-test_monitor_behaviour = Test "monitor_behaviour" "monitor_behaviour.ppd" No Yes Yes (Yes,"")
+test_monitor_behaviour = Test "monitor_behaviour" "monitor_behaviour.ppd" No Yes Yes (Yes,[])
 
 test_pinit_section :: Test
-test_pinit_section = Test "pinit_section" "pinit_section.ppd" No Yes Yes (Yes,"")
+test_pinit_section = Test "pinit_section" "pinit_section.ppd" No Yes Yes (Yes,[])
 
 test_templates_behaviour :: Test
-test_templates_behaviour = Test "templates_behaviour" "templates_behaviour.ppd" No Yes Yes (Yes,"")
+test_templates_behaviour = Test "templates_behaviour" "templates_behaviour.ppd" No Yes Yes (Yes,[])
 
 ------------
 -- Lenses --
